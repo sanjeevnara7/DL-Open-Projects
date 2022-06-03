5cd2db6bd6f23230befaa187e51efe43cdadca98
pycharm-sanjeevnara <sanjeev.nara@gmail.com>
2022-06-03 16:04:30 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def print_init():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Using TensorFlow version: '+tf.__version__)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_init()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
